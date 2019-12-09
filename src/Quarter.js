e7afa0f220139891e4241ea787d0a3bb8d15fc03
damonbogich <damonbogich@gmail.com>
2019-12-09 18:31:19 -0500

    broke up the code for the bottomrow into seperate componenets

---
+++ src/Quarter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const Quarter = () => {
+    return(
+        <div className="quarter">
+        <h3 className="quarter__title">Quarter</h3>
+        <div className="quarter__value">4</div>
+      </div>
+    )
+}
+
+export default Quarter;
